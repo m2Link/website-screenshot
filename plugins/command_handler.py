120c366b5c01885819c533183f7098e59aa9a893
m2Link <88938300+m2Link@users.noreply.github.com>
2021-11-12 21:40:26 +0530

    Update command_handler.py

--- plugins/command_handler.py
+++ plugins/command_handler.py
@@ -55,32 +55,6 @@ async def start(_, message: Message) -> None:
     )
 
 
-@WebshotBot.on_message(filters.command(["help"]))
-async def help(_, message: Message) -> None:
-    await message.reply_text(
-        text="❓ How to use this bot \n \n➤ Send any link to me. \nNote: Link must start with http(s)\n \n➤ Select your preferred settings first. \n \n➤ Then Tap on Start ScreenShot 📸",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "💬 Update Channel",
-                        url="https://t.me/m2botz",
-                    ),
-                    InlineKeyboardButton(
-                        "🗣 Support Group",
-                        url="https://t.me/m2botzsupport",
-                    ),
-                ],
-                [
-                    InlineKeyboardButton(
-                        "🧑‍💻Developer",
-                        url="https://t.me/ask_admin01",
-                    )
-                ],
-            ]
-        ),
-
-
 @WebshotBot.on_message(filters.command(["about", "feedback"]))
 async def feedback(_, message: Message) -> None:
     await message.reply_text(
